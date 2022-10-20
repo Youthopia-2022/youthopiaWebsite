c329fd38102e483a7689c425b8e46530a0c75dff
shubhkhanna <khannashubh2001@gmail.com>
2022-10-21 05:14:44 +0530

    chore: updated typos

--- src/components/Sponsers/index.jsx
+++ src/components/Sponsers/index.jsx
@@ -1,31 +1,49 @@
 import React from "react";
 
-import sbucks from "../../Images/sbucks.png"
-import bking from "../../Images/bking.png"
-import dominose from "../../Images/dominos.png"
-import cocacola from"../../Images/cocacola.png"
-import mcd from "../../Images/macd.png"
+import sbucks from "../../images/sbucks.png";
+import bking from "../../images/bking.png";
+import dominose from "../../images/dominos.png";
+import cocacola from "../../images/cocacola.png";
+import mcd from "../../images/macd.png";
 
 const Sponsers = () => {
-    return (
-        
-        <div class="bg-[#3A3B5C]">
-<div className="sponsers "  >
- 
-   <center> <h3> Our Sponsers</h3 ></center><br/>
-    <div class="flex -2.5">
-    
-        <img className="object-cover h-10 w-10 ml-72 mr-24 mb-8 vertical-align:middle" src={sbucks}   />
-        <img className="object-cover h-10 w-50 ml-6 mr-24  align-middle" src={dominose} />
-        <img className="object-cover h-10 w-50 ml-6 mr-24 item-center " src={bking} />
-        <img className="object-cover h-5 w-50 ml-6 mr-24 mt-2 " src={cocacola} />
-        <img className="object-cover h-10 w-50 ml-6 mr-24 align-cente" src={mcd} />
-</div>
+  return (
+    <div class="bg-[#3A3B5C]">
+      <div className="sponsers ">
+        <center>
+          <h3> Our Sponsers</h3>
+        </center>
+        <br />
+        <div class="flex -2.5">
+          <img
+            className="object-cover h-10 w-10 ml-72 mr-24 mb-8 vertical-align:middle"
+            src={sbucks}
+            alt="sponsers"
+          />
+          <img
+            className="object-cover h-10 w-50 ml-6 mr-24  align-middle"
+            src={dominose}
+            alt="sponsers"
+          />
+          <img
+            className="object-cover h-10 w-50 ml-6 mr-24 item-center "
+            src={bking}
+            alt="sponsers"
+          />
+          <img
+            className="object-cover h-5 w-50 ml-6 mr-24 mt-2 "
+            src={cocacola}
+            alt="sponsers"
+          />
+          <img
+            className="object-cover h-10 w-50 ml-6 mr-24 align-cente"
+            src={mcd}
+            alt="sponsers"
+          />
         </div>
-        </div>
-    )
-}
-    
-        
+      </div>
+    </div>
+  );
+};
 
 export default Sponsers;
--- src/containers/landingContainer/LandingContainer.jsx
+++ src/containers/landingContainer/LandingContainer.jsx
@@ -7,35 +7,35 @@ import "./LandingContainer.css";
 import { useEffect } from "react";
 
 const LandingContainer = () => {
-	useEffect(() => {
-		let vh = window.innerHeight;
-		document.documentElement.style.setProperty("--vh", `${vh}px`);
-	}, []);
+  useEffect(() => {
+    let vh = window.innerHeight;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
 
-	return (
-		<div className="LandingContainer">
-			<img src={ditLogo} alt="" className="ditLogo" />
-			<div className="youthopiaLogo">
-				<img src={hands} alt="" />
-				<div>
-					<p>17th-19th</p>
-					<p>November</p>
-				</div>
-				<Link to="/events" style={{ zIndex: 1 }}>
-					<div className="registerButton">Register Now</div>
-				</Link>
-			</div>
+  return (
+    <div className="LandingContainer">
+      <img src={ditLogo} alt="" className="ditLogo" />
+      <div className="youthopiaLogo">
+        <img src={hands} alt="" />
+        <div>
+          <p>17th-19th</p>
+          <p>November</p>
+        </div>
+        <Link to="/events" style={{ zIndex: 1 }}>
+          <div className="registerButton">Registrations Starting Soon</div>
+        </Link>
+      </div>
 
-			<img
-				src={scrollDownIcon}
-				className="absolute bottom-[1rem] w-[3rem] h-[3rem]"
-				alt=""
-			/>
-			<div className="googlePlay absolute bottom-[1rem] right-[1rem]">
-				<img src={googlePlay} alt="" />
-			</div>
-		</div>
-	);
+      <img
+        src={scrollDownIcon}
+        className="absolute bottom-[1rem] w-[3rem] h-[3rem]"
+        alt=""
+      />
+      <div className="googlePlay absolute bottom-[1rem] right-[1rem]">
+        <img src={googlePlay} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default LandingContainer;

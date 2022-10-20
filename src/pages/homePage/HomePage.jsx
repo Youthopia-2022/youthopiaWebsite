a26fa53076fe37d162b3caa9991dfbc72646e4c8
0xCipher-max <hr607656@gmail.com>
2022-10-20 23:10:03 +0530

    Added DJ Night Page

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom";
+// import "./HomePage.css";
+import image from "../../Images/image-3.png";
 const homePage = () => {
-	return (
-		<div>
-			<h1>This is home page</h1>
-			<ul>
-				<li>about youthopia</li>
-				<li>some important details</li>
-			</ul>
-
-			{/* link to events page */}
-			<Link to="/events">
-				<button> Explore events button</button>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="EventsRoot">
+      <div className="left">
+        <div className="title">DJ NIGHT BY RITIVIZ</div>
+        <div className="image">
+          <img src={image} alt="" />
+        </div>
+        <div className="date">20/11/2022</div>
+        <div className="details">
+          <div className="location">Chanakya Lawn</div>
+          <div className="time">6:20 PM</div>
+        </div>
+        <div className="knowmore">Know More</div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 };
 
 export default homePage;

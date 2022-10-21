ea21bc487804a4d810a83cf151aa76efcaaece26
shubhkhanna <khannashubh2001@gmail.com>
2022-10-21 05:34:33 +0530

    fix: sponers list styling

--- src/components/Sponsers/index.jsx
+++ src/components/Sponsers/index.jsx
@@ -9,34 +9,34 @@ import mcd from "../../images/macd.png";
 const Sponsers = () => {
   return (
     <div class="bg-[#3A3B5C]">
-      <div className="sponsers ">
-        <center>
-          <h3> Our Sponsers</h3>
-        </center>
-        <br />
-        <div class="flex -2.5">
+      <div className="py-4">
+        <h3 className="self-center text-center font-normal text-3xl text-[#00DBFF]">
+          Our Sponsers
+        </h3>
+
+        <div class="flex flex-col md:flex-row my-10 justify-center items-center">
           <img
-            className="object-cover h-10 w-10 ml-72 mr-24 mb-8 vertical-align:middle"
+            className="object-cover h-12 w-50 mx-10 my-5 md:my-0"
             src={sbucks}
             alt="sponsers"
           />
           <img
-            className="object-cover h-10 w-50 ml-6 mr-24  align-middle"
+            className="object-cover h-12 w-50 mx-10 my-5 md:my-0"
             src={dominose}
             alt="sponsers"
           />
           <img
-            className="object-cover h-10 w-50 ml-6 mr-24 item-center "
+            className="object-cover h-12 w-50 mx-10 my-5 md:my-0"
             src={bking}
             alt="sponsers"
           />
           <img
-            className="object-cover h-5 w-50 ml-6 mr-24 mt-2 "
+            className="object-cover h-12 w-50 mx-10 my-5 md:my-0"
             src={cocacola}
             alt="sponsers"
           />
           <img
-            className="object-cover h-10 w-50 ml-6 mr-24 align-cente"
+            className="object-cover h-12 w-50 mx-10 my-5 md:my-0"
             src={mcd}
             alt="sponsers"
           />

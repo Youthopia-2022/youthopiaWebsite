fca23b7b2d44bf74d3acc6a0cb7d9eaedc4c50c1
Nipurn <mehtanipurn222@gmail.com>
2022-10-22 00:04:08 +0530

    Added Events in Home page

--- src/components/eventCard/EventCard.jsx
+++ src/components/eventCard/EventCard.jsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
-import "./EventCard.css";
-const EventCard = (props) => {
+
+
+
+export default function EventCard (props) {
 	return (
 		<div className="EventCard">
-			<h3>{props.title}</h3>
-			<img src={props.image} alt="" />
-			<p>{props.description}</p>
-			<Link to="/events/sampleEvent">
-				<button>More Details</button>
-			</Link>
-		</div>
+      <div className="image">
+        <img src={props.img} alt="" />
+      </div>
+      <div className="r1">
+        <div className="heading">{props.heading}</div>
+        <div></div>
+      </div>
+      <div className="r2">
+        <div className="description">{props.description}</div>
+      </div>
+      <div className="r3">
+        <div>
+        </div>
+        <div>
+        <button className="btn">Explore more</button>
+        </div>
+      </div>
+    </div>
 	);
 };
 
-export default EventCard;
+

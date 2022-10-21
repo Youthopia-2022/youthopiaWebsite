79aba132badbbd5057e3224fd82354a8bca5e270
Vaibhav Aswal <vbhv.aswal@gmail.com>
2022-10-21 13:45:54 +0530

    added individual events pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
 import "./App.css";
 import EventsPage from "./pages/eventsPage/EventsPage";
-import EventDetails from "./pages/eventDetails/EventDetails";
+import EventDetailsTechnical from "./pages/eventDetails/EventDetailsTechnical";
+import EventDetailsCultural from "./pages/eventDetails/EventDetailsCultural";
+import EventDetailsDebate from "./pages/eventDetails/EventDetailsDebate";
+import EventDetailsInformal from "./pages/eventDetails/EventDetailsInformal";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<HomePage />} />;
 			<Route path="/events" element={<EventsPage />} />;
-			<Route path="/events/:id" element={<EventDetails />} />;
+			<Route path="/events/evt" element={<EventDetailsTechnical />} />;
+			<Route path="/events/evc" element={<EventDetailsCultural />} />;
+			<Route path="/events/evd" element={<EventDetailsDebate />} />;
+			<Route path="/events/evi" element={<EventDetailsInformal />} />;
 		</Routes>
 	);
 }
---
+++ src/pages/eventDetails/EventDetailsTechnical.jsx
@@ -0,0 +1,11 @@
+import EventContainer from "../../containers/eventContainer/TechnicalEvent";
+
+const EventDetailsTechnical = () => {
+	return (
+		<div>
+			<EventContainer />
+		</div>
+	);
+};
+
+export default EventDetailsTechnical;

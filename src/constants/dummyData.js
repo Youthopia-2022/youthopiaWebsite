9de5734bc68406216de84dde3876f22d746c3123
shubhkhanna <khannashubh2001@gmail.com>
2022-10-22 08:46:01 +0530

    feat: dummy data

---
+++ src/constants/dummyData.js
@@ -0,0 +1,31 @@
+import image1 from "../images/image1.png";
+import image2 from "../images/image2.png";
+import image3 from "../images/image3.png";
+import image4 from "../images/image4.png";
+
+export const events = [
+  {
+    id: 1,
+    title: "Technical Events",
+    desc: "Fingerstache flexitarian street art 8-bit waistcoat.  Distillery hexagon disrupt edison bulbche.",
+    thumbnail: image1,
+  },
+  {
+    id: 2,
+    title: "Cultural Events",
+    desc: "Fingerstache flexitarian street art 8-bit waistcoat.  Distillery hexagon disrupt edison bulbche.",
+    thumbnail: image2,
+  },
+  {
+    id: 3,
+    title: "Gaming Events",
+    desc: "Fingerstache flexitarian street art 8-bit waistcoat.  Distillery hexagon disrupt edison bulbche.",
+    thumbnail: image3,
+  },
+  {
+    id: 4,
+    title: "Debate Events",
+    desc: "Fingerstache flexitarian street art 8-bit waistcoat.  Distillery hexagon disrupt edison bulbche.",
+    thumbnail: image4,
+  },
+];

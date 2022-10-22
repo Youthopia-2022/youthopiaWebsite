23d7037f70e086d3fd245e28fb2f1aad0b3ba697
Tanishq <tanishqjain1652003@gmail.com>
2022-10-22 16:22:26 +0530

    navbar first commit and updated footer responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import HomePage from "./pages/homePage/HomePage";
 import "./App.css";
 import EventsPage from "./pages/eventsPage/EventsPage";
 import EventDetails from "./pages/eventDetails/EventDetails";
-
+import Footer from './components/Footer/Footer'
+import Navbar from "./components/Navbar/Navbar";
 function App() {
 	return (
+		<>
+		<Navbar/>
 		<Routes>
 			<Route path="/" element={<HomePage />} />;
 			<Route path="/events" element={<EventsPage />} />;
 			<Route path="/events/:id" element={<EventDetails />} />;
 		</Routes>
+		<Footer/>
+		</>
 	);
 }
 

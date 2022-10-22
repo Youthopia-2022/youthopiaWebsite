5b90bceb59d4fe3fe3980949d85dfbf74289faca
Vaibhav Aswal <vbhv.aswal@gmail.com>
2022-10-22 10:54:08 +0530

    fix: added missing required key prop in list items

--- src/components/Events/EventsWrapper.jsx
+++ src/components/Events/EventsWrapper.jsx
@@ -2,19 +2,19 @@ import { events } from "../../constants/dummyData";
 import EventCard from "./EventCard";
 
 const EventsWrapper = () => {
-  return (
-    <div className="EventCard">
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4">
-            {events.map((item) => (
-              <EventCard data={item} />
-            ))}
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+	return (
+		<div className="EventCard">
+			<section className="text-gray-600 body-font">
+				<div className="container px-5 py-24 mx-auto">
+					<div className="flex flex-wrap -m-4">
+						{events.map((item, id) => (
+							<EventCard data={item} key={id} />
+						))}
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default EventsWrapper;

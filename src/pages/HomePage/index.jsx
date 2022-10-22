15d4096b1ccffa0cef33cb628b55b6b7d6fdcbc8
shubhkhanna <khannashubh2001@gmail.com>
2022-10-22 08:53:27 +0530

    chore: update directory name

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,15 @@
+import LandingContainer from "../../containers/landingContainer/LandingContainer";
+import Sponsers from "../../components/Sponsers";
+import Events from "../../components/Events";
+
+const HomePage = () => {
+  return (
+    <>
+      <LandingContainer />
+      <Sponsers />
+      <Events />
+    </>
+  );
+};
+
+export default HomePage;

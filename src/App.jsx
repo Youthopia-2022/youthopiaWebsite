3124ab7f7513597a76e2e7984390b94961a268a9
shubhkhanna <khannashubh2001@gmail.com>
2022-10-22 09:23:39 +0530

    chore: update dthe directory for events

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import EventsPage from "./pages/EventsPage";
-import EventDetails from "./pages/EventsDetails";
-import EventDetailsTechnical from "./pages/eventDetails/EventDetailsTechnical";
-import EventDetailsCultural from "./pages/eventDetails/EventDetailsCultural";
-import EventDetailsDebate from "./pages/eventDetails/EventDetailsDebate";
-import EventDetailsInformal from "./pages/eventDetails/EventDetailsInformal";
+import EventDetailsTechnical from "./pages/EventsDetails/EventDetailsTechnical";
+import EventDetailsCultural from "./pages/EventsDetails/EventDetailsCultural";
+import EventDetailsDebate from "./pages/EventsDetails/EventDetailsDebate";
+import EventDetailsInformal from "./pages/EventsDetails/EventDetailsInformal";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />;
       <Route path="/events" element={<EventsPage />} />;
-      <Route path="/events/:id" element={<EventDetails />} />;
       <Route path="/events/evt" element={<EventDetailsTechnical />} />;
-			<Route path="/events/evc" element={<EventDetailsCultural />} />;
-			<Route path="/events/evd" element={<EventDetailsDebate />} />;
-			<Route path="/events/evi" element={<EventDetailsInformal />} />;
+      <Route path="/events/evc" element={<EventDetailsCultural />} />;
+      <Route path="/events/evd" element={<EventDetailsDebate />} />;
+      <Route path="/events/evi" element={<EventDetailsInformal />} />;
     </Routes>
   );
 };

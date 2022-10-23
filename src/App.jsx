431e55378fb964dfbdb47779a932bc1ae8b24122
shubhkhanna <khannashubh2001@gmail.com>
2022-10-23 11:04:08 +0530

    feat: added navbar and footer in the components

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,23 @@ import EventDetailsTechnical from "./pages/EventsDetails/EventDetailsTechnical";
 import EventDetailsCultural from "./pages/EventsDetails/EventDetailsCultural";
 import EventDetailsDebate from "./pages/EventsDetails/EventDetailsDebate";
 import EventDetailsInformal from "./pages/EventsDetails/EventDetailsInformal";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />;
-      <Route path="/events" element={<EventsPage />} />;
-      <Route path="/events/evt" element={<EventDetailsTechnical />} />;
-      <Route path="/events/evc" element={<EventDetailsCultural />} />;
-      <Route path="/events/evd" element={<EventDetailsDebate />} />;
-      <Route path="/events/evi" element={<EventDetailsInformal />} />;
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />;
+        <Route path="/events" element={<EventsPage />} />;
+        <Route path="/events/evt" element={<EventDetailsTechnical />} />;
+        <Route path="/events/evc" element={<EventDetailsCultural />} />;
+        <Route path="/events/evd" element={<EventDetailsDebate />} />;
+        <Route path="/events/evi" element={<EventDetailsInformal />} />;
+      </Routes>
+      <Footer />
+    </>
   );
 };
 

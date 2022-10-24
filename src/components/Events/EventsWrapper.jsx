92df7be6c025fd4e905c324f2da30ff637211a0c
shubhkhanna <khannashubh2001@gmail.com>
2022-10-24 11:35:06 +0530

    fix homepage events component

--- src/components/Events/EventsWrapper.jsx
+++ src/components/Events/EventsWrapper.jsx
@@ -1,20 +1,20 @@
-import { events } from "../../constants/dummyData";
 import EventCard from "./EventCard";
+import { events } from "../../constants/dummyData";
 
 const EventsWrapper = () => {
-	return (
-		<div className="EventCard">
-			<section className="text-gray-600 body-font">
-				<div className="container px-5 py-24 mx-auto">
-					<div className="flex flex-wrap -m-4">
-						{events.map((item, id) => (
-							<EventCard data={item} key={id} />
-						))}
-					</div>
-				</div>
-			</section>
-		</div>
-	);
+  return (
+    <div className="EventCard">
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            {events.map((item, id) => (
+              <EventCard data={item} key={id} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default EventsWrapper;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,13 +1,15 @@
 import LandingContainer from "../../containers/landingContainer/LandingContainer";
 import Sponsers from "../../components/Sponsers";
 import DjNight from "../../components/EventsRoot/DjNight";
+import Events from "../../components/Events";
+
 const HomePage = () => {
   return (
     <>
       <LandingContainer />
       <Sponsers />
       <DjNight />
-      {/* <Events /> */}
+      <Events />
     </>
   );
 };

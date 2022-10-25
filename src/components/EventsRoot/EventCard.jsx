805176c10cda9ce1bcc3ecaab7cf8198f0631c2b
0xCipher-max <hr607656@gmail.com>
2022-10-25 13:23:52 +0530

    Moved EventCards from /Events to /EventsRoot

---
+++ src/components/EventsRoot/EventCard.jsx
@@ -0,0 +1,25 @@
+import "./EventCard.css";
+import "@fontsource/ibm-plex-sans";
+const EventCard = (props) => {
+  return (
+    <div className="EventCard">
+      <div className="imagee">
+        <img src={props.image} alt="" />
+      </div>
+      <div className="r1">
+        <div className="locationn">{props.location}</div>
+        <div className="datee">{props.date}</div>
+      </div>
+      <div className="r2">
+        <div className="pricee">{props.price}</div>
+        <div className="timee">{props.time}</div>
+      </div>
+      <div className="r3">
+        <div className="namee">{props.name}</div>
+        <div className="buttonn">Register Now</div>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;

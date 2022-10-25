e9c9c8b76760cc68dd5184261408fef483b2c391
shubhkhanna <khannashubh2001@gmail.com>
2022-10-26 02:52:36 +0530

    fix: about us page design

--- src/components/Abus/Abus.jsx
+++ src/components/Abus/Abus.jsx
@@ -1,17 +1,27 @@
-import React from "react";
 import "./Abus.css";
-function Abus(props) {
-	return (
-		<div>
-			<p className="heading">{props.title}</p>
-			{props.members.map((member, id) => (
-				<div key={id}>
-					<p className="base">{member.membername}</p>
-					<p className="base2">{member.branchyear}</p>
-				</div>
-			))}
-		</div>
-	);
-}
+
+const Abus = (props) => {
+  return (
+    <div>
+      <p className="heading mb-4 capitalize">{props.title}</p>
+      {props.heads.length > 0 && (
+        <>
+          {props.heads.map((item, id) => (
+            <div key={id} className="my-1">
+              <p className="base">{item.membername}</p>
+              <p className="base2">{item.branchyear}</p>
+            </div>
+          ))}
+        </>
+      )}
+      {props.members.map((member, id) => (
+        <div key={id}>
+          <p className="base">{member.membername}</p>
+          <p className="base2">{member.branchyear}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Abus;
--- src/pages/Aboutus/Aboutus.jsx
+++ src/pages/Aboutus/Aboutus.jsx
@@ -3,40 +3,71 @@ import absimage from "../../images/absimage.png";
 import logo from "../../images/logo.png";
 import "./Aboutus.css";
 import Abus from "../../components/Abus/Abus";
-const Aboutus = () => {
-	const myStyle7 = {
-		color: "white",
-		fontFamily: "'IBM Plex Sans'",
-		fontSize: 28,
-		fontWeight: 700,
-	};
-	const Conveners = [
-		{ membername: "abc", branchyear: "branch" },
-		{ membername: "abc", branchyear: "branch" },
-	];
-	const DesignTeam = [
-		{ membername: "abc", branchyear: "branch" },
-		{ membername: "abc", branchyear: "branch" },
-		{ membername: "abc", branchyear: "branch" },
-		{ membername: "abc", branchyear: "branch" },
-	];
 
-	return (
-		<div className=" maincontainerAboutUs bg-[#3A3B5C]">
-			<div className="aboutUsContent">
-				<h1 style={myStyle7}>Meet Our Team</h1>
-				<br></br>
-				<div className="teamsHolder">
-					<Abus title="Conveners" members={Conveners} />
-					<Abus title="Designer team" members={DesignTeam} />
-					<Abus title="Designer Team members" members={Conveners} />
-					<Abus title="Development Team" members={Conveners} />
-					<Abus title="Development Team members" members={Conveners} />
-				</div>
-			</div>
-			<img className="logo" src={logo} alt="img not availabe"></img>
-			<img className="absimage" src={absimage} alt="img not avilable"></img>
-		</div>
-	);
+const AllTeams = [
+  {
+    "Designing Team": {
+      heads: [
+        { membername: "John Doe (Head)", branchyear: "CSE" },
+        { membername: "John Doe (Head)", branchyear: "CSE" },
+      ],
+      members: [
+        { membername: "Jack", branchyear: "CSE" },
+        { membername: "Jack", branchyear: "CSE" },
+        { membername: "Jack", branchyear: "CSE" },
+        { membername: "Jack", branchyear: "CSE" },
+      ],
+    },
+  },
+  {
+    "Technical Team": {
+      heads: [
+        { membername: "John Doe (Head)", branchyear: "CSE" },
+        { membername: "John Doe", branchyear: "CSE" },
+      ],
+      members: [
+        { membername: "John Doe", branchyear: "CSE" },
+        { membername: "John Doe", branchyear: "CSE" },
+        { membername: "John Doe", branchyear: "CSE" },
+        { membername: "John Doe", branchyear: "CSE" },
+      ],
+    },
+  },
+];
+
+const Aboutus = () => {
+  const myStyle7 = {
+    color: "white",
+    fontFamily: "'IBM Plex Sans'",
+    fontSize: 28,
+    fontWeight: 700,
+  };
+  const Conveners = [
+    { membername: "Jack", branchyear: "CSE" },
+    { membername: "Jack", branchyear: "CSE" },
+  ];
+
+  return (
+    <div className="maincontainerAboutUs bg-[#3A3B5C]">
+      <div className="aboutUsContent">
+        <h1 style={myStyle7}>Meet Our Team</h1>
+        <br></br>
+        <div className="teamsHolder">
+          <Abus title="Conveners" heads={[]} members={Conveners} />
+
+          {AllTeams.map((team, id) => (
+            <Abus
+              key={id}
+              title={Object.keys(team)[0]}
+              heads={team[Object.keys(team)[0]].heads}
+              members={team[Object.keys(team)[0]].members}
+            />
+          ))}
+        </div>
+      </div>
+      <img className="logo" src={logo} alt="img not availabe"></img>
+      <img className="absimage" src={absimage} alt="img not avilable"></img>
+    </div>
+  );
 };
 export default Aboutus;

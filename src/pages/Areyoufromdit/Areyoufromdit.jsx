50913f9a4a827e1bccbd176d9f21331a863abb91
shreyal0007 <shreyaljain0007@gmail.com>
2022-10-26 22:16:53 +0530

    are you from dit responsive1

--- src/pages/Areyoufromdit/Areyoufromdit.jsx
+++ src/pages/Areyoufromdit/Areyoufromdit.jsx
@@ -7,7 +7,43 @@ const fromdit = () =>{
     return(
         <div className="maincontainer">
         <div style={{backgroundImage: `url(${registerbackground})`,}}>
-            <div className="flex flex-row">
+            <div style={{display:'flex'}}>
+                <div>
+                <img className="w=72 h=72  pl-5 pt-5"src={sampleimage1}/>
+                </div>
+
+                <div style={{width:'100%'}}>
+                    <div className="class1 mt-9">
+                        <div>Technical Events</div>
+                    </div>
+                    <div className="class2"
+                    style={{display:'flex', justifyContent:'center'}}>
+                        <div style={{width:'100%'}}>Vedanta Hall</div>
+                        <div style={{width:'100%'}}>21/21/2022</div>
+                        <div style={{width:'100%'}}>13:07</div>
+                    </div>
+                    <div className="class3"
+                    style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+                        <div style={{justifyContent:'flex-start', width:'100%'}}>
+                        <p>EventCoordinator:Joestar Jotare</p>
+                        <p>Over All Head:Dio Brando</p>
+                        </div>
+                        <div style={{justifyContent:'flex-end', width:'100%', display:'flex', marginRight:'12px'}}>
+                           Frees:300/Head
+                        </div>
+                    </div>
+                    {/* <div className="class3 flex flex-row space-x-72  ">
+                        <div>
+                        <p>EventCoordinator:Joestar Jotare</p>
+                        <p>Over All Head:Dio Brando</p>
+                        </div>
+                        <div >
+                            <p>Frees:300/Head</p>
+                        </div>
+                    </div> */}
+                </div>
+            </div>
+            {/* <div className="flex flex-row">
                 <div>
                 <img className="w=72 h=72 pl-12 pt-10"src={sampleimage1}/>
                 </div>
@@ -31,17 +67,18 @@ const fromdit = () =>{
                     </div>
                 </div>
 
-            </div>
-            <br/><br/><br/><br/><br/>
-            <div className="flex flex-col">
+            </div> */}
+            {/* <br/><br/><br/><br/><br/> */}
+            <br></br>
+            <div>
                 <div className="class5">
                     <p>Are you From DIT University?</p>
                 </div>
-                <div className="  flex felx-row space-x-12">
-                    <div className="button1">
+                <div className="flex flex-row  ">
+                    <div className="button1 ml-5 mr-5 ">
                         <button><p className="buttonfont">Yes</p></button>
                     </div>
-                    <div className="button2">    
+                    <div className="button2 mr-5  "> 
                     <button><p className="buttonfont">No</p></button>
                     </div>
                 </div>

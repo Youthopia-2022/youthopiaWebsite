0bd02b43d6852c2abe12d0bb023db1f7580ecd4e
shubhkhanna <khannashubh2001@gmail.com>
2022-10-27 09:51:59 +0530

    fix: imports in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,21 @@ import EventDetailsCultural from "./pages/EventsDetails/EventDetailsCultural";
 import EventDetailsDebate from "./pages/EventsDetails/EventDetailsDebate";
 import EventDetailsInformal from "./pages/EventsDetails/EventDetailsInformal";
 import Areyoufromdit from "./pages/Areyoufromdit/Areyoufromdit";
-
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Aboutus from "./pages/Aboutus/Aboutus";
-import EventRegisterForm from "./components/eventRegisteForm/EventRegisterForm";
+import RegisterIndv from "./components/eventRegisteForm/RegisterIndv";
+import RegisterIndvdit from "./components/eventRegisteForm/RegisterIndvdit";
+
 const useScrollToTop = () => {
-	const location = useLocation();
-	useEffect(() => {
-		window.scrollTo({ top: 0 });
-		// scroll to the top of the browser window when changing route
-		// the window object is a normal DOM object and is safe to use in React.
-	}, [location]);
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+    // scroll to the top of the browser window when changing route
+    // the window object is a normal DOM object and is safe to use in React.
+  }, [location]);
 };
+
 const App = () => {
   useScrollToTop();
   return (
@@ -48,8 +50,8 @@ const App = () => {
           element={<EventDetailsInformal />}
         ></Route>
         <Route path="/gallery" element={<Gallery />} />;
-        <Route path="/forms/indv/dit" element={<RegisterIndvdit/>} />;
-        <Route path="/forms/indv/nondit" element={<RegisterIndv/>} />;
+        <Route path="/forms/indv/dit" element={<RegisterIndvdit />} />;
+        <Route path="/forms/indv/nondit" element={<RegisterIndv />} />;
         <Route path="/aboutus" element={<Aboutus />} />;
         <Route path="/fromdit" element={<Areyoufromdit />} />;
       </Routes>

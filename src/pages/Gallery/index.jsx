57f136b2042e6bc7226611ddba03f5de605b7d1e
shreyal0007 <shreyaljain0007@gmail.com>
2022-11-03 13:58:04 +0530

    gallery modified

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -24,7 +24,20 @@ import gimage23 from "../../images/gimage23-min.webp";
 import gimage24 from "../../images/gimage24-min.webp";
 import gimage25 from "../../images/gimage25-min.webp";
 import gimage26 from "../../images/gimage26-min.webp";
+import gimage27 from "../../images/gimage27-min.webp";
+import gimage28 from "../../images/gimage28-min.webp";
+import gimage29 from "../../images/gimage29-min.webp";
+import gimage30 from "../../images/gimage30-min.webp";
+import gimage31 from "../../images/gimage31-min.webp";
+import gimage32 from "../../images/gimage32-min.webp";
+import gimage33 from "../../images/gimage33-min.webp";
+import gimage34 from "../../images/gimage34-min.webp";
+import gimage35 from "../../images/gimage35-min.webp";
+import gimage36 from "../../images/gimage36-min.webp";
+import gimage37 from "../../images/gimage37-min.webp";
+import gimage38 from "../../images/gimage38-min.webp";
 
+import CountDownThumbnail from "../../images/CountDownThumbnail.webp";
 
 const Gallery = () => {
 	return (
@@ -37,19 +50,19 @@ const Gallery = () => {
 					<h3 className="text-white text-6xl font-bold">REMEMBER</h3>
 					<h3 className="text-[#3F2EA1] text-6xl font-bold">YOUR</h3>
 					<h3 className="text-[#3F2EA1] text-6xl font-bold">ENTIRE LIFE</h3>
-					<img className="h-96 w-96 mt-10" src={gimage1} alt="galleryImage" />
+					<img className="h-96 w-96 mt-10" src={gimage4} alt="galleryImage" />
 				</div>
                 
 				{/* Second Column Images */}
 				<div className="flex flex-col mt-8 md:mt-0 md:ml-10">
 					<img className="h-96 w-auto" src={gimage2} alt="galleryImage" />
-					<img className="h-96 w-auto mt-10" src={gimage3} alt="galleryImage" />
+					<img className="h-96 w-full mt-10" src={gimage3} alt="galleryImage" />
 				</div>
 			</div>
 
 			{/* Second Row */}
 			<div className="px-5 md:px-10 py-10">
-				<img className="h-auto w-full" src={gimage4} alt="galleryImage" />
+				<img className="h-auto w-full" src={CountDownThumbnail} alt="galleryImage" />
 			</div>
 
 			{/* Third Row */}
@@ -148,23 +161,7 @@ const Gallery = () => {
 				src={gimage20}
 				alt="galleryImage"
 			/>
-			<div className="flex flex-col md:flex-row w-full items-center">
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage21}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage22}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage23}
-					alt="galleryImage"
-				/>
-			</div>
+			
 			<img
 				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
 				src={gimage24}
@@ -181,6 +178,68 @@ const Gallery = () => {
 				src={gimage26}
 				alt="galleryImage"
 			/>
+            <div className="flex flex-col md:flex-row w-full items-center">
+				<img
+					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+					src={gimage27}
+					alt="galleryImage"
+				/>
+				<img
+					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+					src={gimage28}
+					alt="galleryImage"
+				/>
+				<img
+					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+					src={gimage29}
+					alt="galleryImage"
+				/>
+			</div>
+            <img
+				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+				src={gimage30}
+				alt="galleryImage"
+			/>
+            <div className="flex flex-col md:flex-row w-full items-center">
+				<img
+					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+					src={gimage31}
+					alt="galleryImage"
+				/>
+				<img
+					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+					src={gimage32}
+					alt="galleryImage"
+				/>
+				<img
+					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+					src={gimage37}
+					alt="galleryImage"
+				/>
+			</div>
+            <img
+				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+				src={gimage38}
+				alt="galleryImage"
+			/>
+            <div className="flex flex-col md:flex-row w-full items-center">
+				<img
+					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+					src={gimage35}
+					alt="galleryImage"
+				/>
+				<img
+					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+					src={gimage34}
+					alt="galleryImage"
+				/>
+				<img
+					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+					src={gimage33}
+					alt="galleryImage"
+				/>
+            
+			</div>
 		</div>
 	);
 };

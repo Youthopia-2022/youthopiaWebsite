8c46837570ac4249bc79936dd5c44c162e207c41
Vaibhav Aswal <vbhv.aswal@gmail.com>
2022-11-07 21:20:03 +0530

    added start night

---
+++ src/components/startNight/StartNight.jsx
@@ -0,0 +1,16 @@
+import st from "../../images/StarNight.png";
+import st2 from "../../images/starNight2.png";
+import "./StartNight.css";
+
+const StartNight = () => {
+	return (
+		<div className="starNight">
+			<img src={st} alt="" className="starNightImage" />
+			<div className="starNightImage2">
+				<img src={st2} alt="" />
+			</div>
+		</div>
+	);
+};
+
+export default StartNight;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,15 +1,17 @@
 import LandingContainer from "../../containers/landingContainer/LandingContainer";
 import Sponsers from "../../components/Sponsers";
-import DjNight from "../../components/EventsRoot/DjNight";
+// import DjNight from "../../components/EventsRoot/DjNight";
 import Events from "../../components/Events";
 import CountDown from "../../containers/CountDown/CountDown";
+import StartNight from "../../components/startNight/StartNight";
 
 const HomePage = () => {
 	return (
 		<>
 			<LandingContainer />
 			<Sponsers />
-			<DjNight />
+			{/* <DjNight /> */}
+			<StartNight />
 			<Events />
 			<CountDown />
 		</>

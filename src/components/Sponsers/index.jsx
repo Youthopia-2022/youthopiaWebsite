ee9ee9d51bb8e57fe6a082480264085b0db5a896
shreyal0007 <shreyaljain0007@gmail.com>
2022-11-07 19:32:42 +0530

    star night image

--- src/components/Sponsers/index.jsx
+++ src/components/Sponsers/index.jsx
@@ -43,7 +43,7 @@ const Sponsers = () => {
   }, []);
 
   return (
-    <div className="bg-[#3A3B5C]">
+    <div className="bg-[#3A3B5]">
       <div className="py-4 md:py-10">
         <h3 className="self-center text-center font-normal text-3xl text-[#FFF]">
           Our Sponsors
@@ -55,7 +55,7 @@ const Sponsers = () => {
               return (
                 <img
                   key={id}
-                  className="object-cover h-12 space-x-8 w-50 mx-10 my-5  md:my-0 sponser1"
+                  className="object-cover h-12  w-50 mx-10 my-5  md:my-0 sponser1"
                   src={sponsors.sponsor_icon_url}
                   alt="sponsers"
                 />

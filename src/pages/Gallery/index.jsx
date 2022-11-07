d27619ea1c1289bfcae123f011d9ca27529a55a7
Vaibhav Aswal <vbhv.aswal@gmail.com>
2022-11-07 16:15:01 +0530

    design changes in gallery

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -45,14 +45,17 @@ const Gallery = () => {
 			<div className="flex flex-col md:flex-row items-center justify-center px-5 md:px-10 w-full">
 				{/* First Column Images */}
 				<div className="flex flex-col">
-					<h3 className="text-white text-6xl font-bold">A NIGHT</h3>
-					<h3 className="text-white text-6xl font-bold">TO</h3>
-					<h3 className="text-white text-6xl font-bold">REMEMBER</h3>
-					<h3 className="text-[#3F2EA1] text-6xl font-bold">YOUR</h3>
-					<h3 className="text-[#3F2EA1] text-6xl font-bold">ENTIRE LIFE</h3>
+					<div className="h-96 flex flex-col">
+						{" "}
+						<h3 className="text-white text-6xl font-bold">A NIGHT</h3>
+						<h3 className="text-white text-6xl font-bold">TO</h3>
+						<h3 className="text-white text-6xl font-bold">REMEMBER</h3>
+						<h3 className="text-[#3F2EA1] text-6xl font-bold">YOUR</h3>
+						<h3 className="text-[#3F2EA1] text-6xl font-bold">ENTIRE LIFE</h3>
+					</div>
 					<img className="h-96 w-auto mt-10" src={gimage4} alt="galleryImage" />
 				</div>
-                
+
 				{/* Second Column Images */}
 				<div className="flex flex-col mt-8 md:mt-0 md:ml-10">
 					<img className="h-96 w-auto" src={gimage2} alt="galleryImage" />
@@ -60,198 +63,204 @@ const Gallery = () => {
 				</div>
 			</div>
 
-			{/* Second Row */}
-			<div className="px-5 md:px-10 py-10">
-				<img className="h-auto w-full" src={CountDownThumbnail} alt="galleryImage" />
-			</div>
+			<div className="flex flex-col w-full justify-center items-center">
+				<div className="max-w-[1269px]">
+					{/* Second Row */}
+					<div className="px-5 md:px-10 py-10 ">
+						<img
+							className="h-auto w-full"
+							src={CountDownThumbnail}
+							alt="galleryImage"
+						/>
+					</div>
 
-			{/* Third Row */}
-			<h2 className="px-10 pb-10 text-[#D44865] font-bold text-5xl md:text-7xl text-center">
-				DO JOIN US FOR THE GRAND EVENT
-			</h2>
+					{/* Third Row */}
+					<h2 className="px-10 pb-10 text-[#D44865] font-bold text-5xl md:text-7xl text-center">
+						DO JOIN US FOR THE GRAND EVENT
+					</h2>
 
-			<div className="flex flex-col md:flex-row w-full items-center">
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage5}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage6}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage7}
-					alt="galleryImage"
-				/>
-			</div>
+					<div className="flex flex-col md:flex-row w-full items-center">
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage5}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage6}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage7}
+							alt="galleryImage"
+						/>
+					</div>
 
-			<img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage8}
-				alt="galleryImage"
-			/>
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage8}
+						alt="galleryImage"
+					/>
 
-			<img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage9}
-				alt="galleryImage"
-			/>
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage9}
+						alt="galleryImage"
+					/>
 
-			
-			<div className="flex flex-col md:flex-row w-full items-center">
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage11}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage12}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage13}
-					alt="galleryImage"
-				/>
-			</div>
-			<img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage14}
-				alt="galleryImage"
-			/>
+					<div className="flex flex-col md:flex-row w-full items-center">
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage11}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage12}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage13}
+							alt="galleryImage"
+						/>
+					</div>
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage14}
+						alt="galleryImage"
+					/>
 
-			<img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage15}
-				alt="galleryImage"
-			/>
-			<div className="flex flex-col md:flex-row w-full items-center">
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage16}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage17}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage18}
-					alt="galleryImage"
-				/>
-			</div>
-			<img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage19}
-				alt="galleryImage"
-			/>
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage15}
+						alt="galleryImage"
+					/>
+					<div className="flex flex-col md:flex-row w-full items-center">
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage16}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage17}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage18}
+							alt="galleryImage"
+						/>
+					</div>
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage19}
+						alt="galleryImage"
+					/>
 
-			<img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage20}
-				alt="galleryImage"
-			/>
-			
-			<img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage24}
-				alt="galleryImage"
-			/>
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage20}
+						alt="galleryImage"
+					/>
 
-			<img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage25}
-				alt="galleryImage"
-			/>
-			<img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage26}
-				alt="galleryImage"
-			/>
-            <div className="flex flex-col md:flex-row w-full items-center">
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage27}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage28}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage29}
-					alt="galleryImage"
-				/>
-			</div>
-            <img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage30}
-				alt="galleryImage"
-			/>
-            <div className="flex flex-col md:flex-row w-full items-center">
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage31}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage32}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage37}
-					alt="galleryImage"
-				/>
-			</div>
-            <img
-				className="w-full h-auto object-contain px-5 md:px-10 mt-10"
-				src={gimage38}
-				alt="galleryImage"
-			/>
-            <div className="flex flex-col md:flex-row w-full items-center">
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage35}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage34}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage33}
-					alt="galleryImage"
-				/>
-            
-			</div>
-            <div className="flex flex-col md:flex-row w-full items-center">
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage21}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage22}
-					alt="galleryImage"
-				/>
-				<img
-					className="py-5 px-5 md:px-10 md:w-1/3 h-30"
-					src={gimage23}
-					alt="galleryImage"
-				/>
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage24}
+						alt="galleryImage"
+					/>
+
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage25}
+						alt="galleryImage"
+					/>
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage26}
+						alt="galleryImage"
+					/>
+					<div className="flex flex-col md:flex-row w-full items-center">
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage27}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage28}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage29}
+							alt="galleryImage"
+						/>
+					</div>
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage30}
+						alt="galleryImage"
+					/>
+					<div className="flex flex-col md:flex-row w-full items-center">
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage31}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage32}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage37}
+							alt="galleryImage"
+						/>
+					</div>
+					<img
+						className="w-full h-auto object-contain px-5 md:px-10 mt-10"
+						src={gimage38}
+						alt="galleryImage"
+					/>
+					<div className="flex flex-col md:flex-row w-full items-center">
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage35}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage34}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage33}
+							alt="galleryImage"
+						/>
+					</div>
+					<div className="flex flex-col md:flex-row w-full items-center">
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage21}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage22}
+							alt="galleryImage"
+						/>
+						<img
+							className="py-5 px-5 md:px-10 md:w-1/3 h-30"
+							src={gimage23}
+							alt="galleryImage"
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
 	);

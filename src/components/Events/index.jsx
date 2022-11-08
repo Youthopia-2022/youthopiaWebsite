ea85d0d751fe1ec4c724d5a23c50d5ccf54721dd
shubhkhanna <khannashubh2001@gmail.com>
2022-11-08 13:00:51 +0530

    fix: styling

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -8,7 +8,7 @@ const Events = () => {
       <div className="flex flex-wrap w-full">
         <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-[#1BBEE9]">
-            Upcomming Bonanzas !!
+            Upcoming Bonanzas !!
           </h1>
         </div>
         <Link
